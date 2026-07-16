0f88f6d871154d9bedb4649a9879d9a17faf5434
diegodelvalle3 <diegorogito@outlook.com>
2026-07-16 10:40:39 -0600

    feat: members data

---
+++ src/data/miembros.ts
@@ -0,0 +1,77 @@
+import diego from "../images/ayudamigos/ayudamigo_diego.png";
+import iyari from "../images/ayudamigos/ayudamigo_iyari.png";
+import dani from "../images/ayudamigos/ayudamigo_dani.png";
+import kenia from "../images/ayudamigos/ayudamiga_kenia.png";
+import coral from "../images/ayudamigos/ayudamigo_coral.png";
+
+import aymiga_sinfoto from "../images/ayudamigos/ayudamiga_sinfoto.png";
+import ayudamigos_sinfoto from "../images/ayudamigos/ayudamigo_sinfoto.png";
+
+export const miembros = [
+    {
+        nombre: "Diego del Valle",
+        img_url: diego,
+        miembro_desde: "10 Agosto 2022",
+        rol: "Fundador",
+    },
+    {
+        nombre: "Kenia Dominguez",
+        img_url: kenia,
+        miembro_desde: "03 Octubre 2022",
+        rol: "Ayudamiga",
+    },
+    {
+        nombre: "Daniel Bahena",
+        img_url: dani,
+        miembro_desde: "23 Enero 2024",
+        rol: "Digital",
+    },
+    {
+        nombre: "Iyari Tirado",
+        img_url: iyari,
+        miembro_desde: "03 Octubre 2022",
+        rol: "Ayudamiga",
+    },
+    {
+        nombre: "Coral Hernández",
+        img_url: coral,
+        miembro_desde: "03 Octubre 2022",
+        rol: "Ayudamiga",
+    },
+    {
+        nombre: "Ángel Rivas",
+        img_url: ayudamigos_sinfoto,
+        miembro_desde: "23 Noviembre 2022",
+        rol: "Ayudamigo",
+    },
+    {
+        nombre: "Victor Ceballos",
+        img_url: ayudamigos_sinfoto,
+        miembro_desde: "13 Julio 2024",
+        rol: "Ayudamigo",
+    },
+    {
+        nombre: "Enrique Rodríguez",
+        img_url: ayudamigos_sinfoto,
+        miembro_desde: "03 Octubre 2022",
+        rol: "Digital",
+    },
+    {
+        nombre: "Melvin Sánchez",
+        img_url: ayudamigos_sinfoto,
+        miembro_desde: "05 Octubre 2022",
+        rol: "Ayudamigo",
+    },
+    {
+        nombre: "Danna Hurtado",
+        img_url: aymiga_sinfoto,
+        miembro_desde: "05 Octubre 2022",
+        rol: "Ayudamiga",
+    },
+    {
+        nombre: "Roberto García",
+        img_url: ayudamigos_sinfoto,
+        miembro_desde: "03 Octubre 2022",
+        rol: "Ayudamigo",
+    },
+];
